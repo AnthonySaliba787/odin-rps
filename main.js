bbf497a59b9158507deb9a461911238012e5998b
Anthony <anthonysaliba93@gmail.com>
2022-08-23 11:45:38 +0300

    Make the function that gives a random computer choice

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+function getComputerChoice() {
+  choices = ["rock", "paper", "scissors"];
+  choice = choices[Math.floor(Math.random() * choices.length)];
+  return choice;
+}
 
+function mainRound(playerSelection, computerSelection) {}
