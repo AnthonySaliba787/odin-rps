2419327b365c881a226b5f6fbbf723884671a3f6
Anthony <anthonysaliba93@gmail.com>
2022-08-24 10:05:24 +0300

    Rework the entire system for UI use

--- main.js
+++ main.js
@@ -5,45 +5,168 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let plrSel = playerSelection.toLowerCase();
-  if (plrSel == computerSelection) {
-    return [
-      console.log("Draw! " + plrSel + " vs " + computerSelection + "."),
-      2,
-    ];
-  } else if (plrSel == "rock" && computerSelection == "paper") {
-    return [
-      console.log("You lose! " + plrSel + " vs " + computerSelection + "."),
-      0,
-    ];
-  } else if (plrSel == "rock" && computerSelection == "scissors") {
-    return [
-      console.log("You win! " + plrSel + " vs " + computerSelection + "."),
-      1,
-    ];
-  } else if (plrSel == "paper" && computerSelection == "rock") {
-    return [
-      console.log("You win! " + plrSel + " vs " + computerSelection + "."),
-      1,
-    ];
-  } else if (plrSel == "paper" && computerSelection == "scissors") {
-    return [
-      console.log("You lose! " + plrSel + " vs " + computerSelection + "."),
-      0,
-    ];
-  } else if (plrSel == "scissors" && computerSelection == "paper") {
-    return [
-      console.log("You win! " + plrSel + " vs " + computerSelection + "."),
-      1,
-    ];
-  } else if (plrSel == "scissors" && computerSelection == "rock") {
-    return [
-      console.log("You lose! " + plrSel + " vs " + computerSelection + "."),
-      0,
-    ];
+  let result = 0;
+  if (playerSelection == computerSelection) {
+    return [playerSelection, computerSelection, (result = 2)];
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return [playerSelection, computerSelection, (result += 0)];
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return [playerSelection, computerSelection, (result = 1)];
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return [playerSelection, computerSelection, (result = 1)];
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return [playerSelection, computerSelection, (result = 0)];
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return [playerSelection, computerSelection, (result = 1)];
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return [playerSelection, computerSelection, (result = 0)];
   }
 }
 
-function game() {}
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorButton = document.querySelector(".scissor");
+const mainDiv = document.querySelector(".info");
+const mainResult = document.querySelector(".results");
 
-game();
+let points = 0;
+let playerScore = 0;
+let botScore = 0;
+let round = 0;
+let playerSelection = "";
+
+rockButton.addEventListener("click", () => {
+  mainResult.textContent = "";
+  playerSelection = "rock";
+  const [playerValue, botValue, gameDecision] = playRound(
+    playerSelection,
+    getComputerChoice()
+  );
+
+  if (gameDecision == 0) {
+    mainDiv.textContent =
+      "You lost this round! " + playerValue + " vs. " + botValue + ".";
+    botScore += 1;
+    round += 1;
+  } else if (gameDecision == 1) {
+    mainDiv.textContent =
+      "You won this round! " + playerValue + " vs. " + botValue + ".";
+    playerScore += 1;
+    round += 1;
+  } else {
+    mainDiv.textContent = "Draw! " + playerValue + " vs. " + botValue + ".";
+    round += 1;
+  }
+
+  if (round >= 5) {
+    if (playerScore > botScore) {
+      mainResult.textContent =
+        "You won this match! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    } else if (botScore > playerScore) {
+      mainResult.textContent =
+        "You lost this match! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    } else {
+      mainResult.textContent =
+        "This match is a draw! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    }
+  }
+});
+paperButton.addEventListener("click", () => {
+  mainResult.textContent = "";
+  playerSelection = "paper";
+  const [playerValue, botValue, gameDecision] = playRound(
+    playerSelection,
+    getComputerChoice()
+  );
+
+  if (gameDecision == 0) {
+    mainDiv.textContent =
+      "You lost this round! " + playerValue + " vs. " + botValue + ".";
+    botScore += 1;
+    round += 1;
+  } else if (gameDecision == 1) {
+    mainDiv.textContent =
+      "You won this round! " + playerValue + " vs. " + botValue + ".";
+    playerScore += 1;
+    round += 1;
+  } else {
+    mainDiv.textContent = "Draw! " + playerValue + " vs. " + botValue + ".";
+    round += 1;
+  }
+
+  if (round >= 5) {
+    if (playerScore > botScore) {
+      mainResult.textContent =
+        "You won this match! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    } else if (botScore > playerScore) {
+      mainResult.textContent =
+        "You lost this match! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    } else {
+      mainResult.textContent =
+        "This match is a draw! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    }
+  }
+});
+scissorButton.addEventListener("click", () => {
+  mainResult.textContent = "";
+  playerSelection = "scissors";
+  const [playerValue, botValue, gameDecision] = playRound(
+    playerSelection,
+    getComputerChoice()
+  );
+
+  if (gameDecision == 0) {
+    mainDiv.textContent =
+      "You lost this round! " + playerValue + " vs. " + botValue + ".";
+    botScore += 1;
+    round += 1;
+  } else if (gameDecision == 1) {
+    mainDiv.textContent =
+      "You won this round! " + playerValue + " vs. " + botValue + ".";
+    playerScore += 1;
+    round += 1;
+  } else {
+    mainDiv.textContent = "Draw! " + playerValue + " vs. " + botValue + ".";
+    round += 1;
+  }
+
+  if (round >= 5) {
+    if (playerScore > botScore) {
+      mainResult.textContent =
+        "You won this match! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    } else if (botScore > playerScore) {
+      mainResult.textContent =
+        "You lost this match! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    } else {
+      mainResult.textContent =
+        "This match is a draw! " + playerScore + " vs. " + botScore + ".";
+      botScore = 0;
+      playerScore = 0;
+      round = 0;
+    }
+  }
+});
